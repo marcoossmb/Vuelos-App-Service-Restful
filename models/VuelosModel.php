1d3dd72c2c3ebc883154c9969792f6695de7d201
marcos <marcossblazquezz@gmail.com>
2024-02-19 19:48:31 +0100

    change query to see a flight

--- models/VuelosModel.php
+++ models/VuelosModel.php
@@ -13,7 +13,11 @@ class VuelosModel extends DB {
     // Devuelve un array departamento
     public function getUnVuelo($nuvuel) {
         try {
-            $sql = "SELECT * FROM $this->table WHERE identificador=?";
+            $sql = "SELECT v.identificador, ao.codaeropuerto AS 'aeroorigen', ao.nombre AS 'aeronameorigen', ao.pais AS 'paisorigen',
+                    ae.codaeropuerto AS 'aerodestino', ae.nombre AS 'aeronamedestino', ae.pais AS 'paisdestino', v.tipovuelo, COUNT(p.idpasaje) AS 'idpasaje' 
+                    FROM vuelo v JOIN aeropuerto ao ON v.aeropuertoorigen = ao.codaeropuerto JOIN aeropuerto ae 
+                    ON v.aeropuertodestino = ae.codaeropuerto LEFT JOIN pasaje p ON v.identificador = p.identificador WHERE v.identificador=?
+                    GROUP BY v.identificador;";
             $sentencia = $this->conexion->prepare($sql);
             $sentencia->bindParam(1, $nuvuel);
             $sentencia->execute();
